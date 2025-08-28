419133b3089b13593f05c7fec4290b339af30848
jef <jef@unknown.unknown>
2025-08-28 10:27:52 -0500

    update some styles at home

--- public/home.php
+++ public/home.php
@@ -4,4 +4,15 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: signin.php");
     exit;
 }
+if (isset($_GET['css'])) {
+    $file = realpath(__DIR__ . '/../server/protected/css/' . basename($_GET['css']));
+    if ($file && file_exists($file)) {
+        header("Content-Type: text/css");
+        readfile($file);
+        exit;
+    } else {
+        http_response_code(404);
+        exit;
+    }
+}
 include "../server/protected/home.php";
